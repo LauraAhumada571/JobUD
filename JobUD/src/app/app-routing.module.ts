d56c8af36747ea21c847a91c8bb7fb3f1f0c3d2e
Laura Ahumada <lauraahumada571@gmail.com>
2019-05-06 01:46:53 -0500

    sb admin2

--- JobUD/src/app/app-routing.module.ts
+++ JobUD/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '../app/Component/login/login.component';
+import {RegisterComponent } from '../app/Component/register/register.component';
 import { AppComponent } from './app.component';
  
 const routes: Routes = [];
@@ -9,6 +10,8 @@ export const rootRouterConfig: Routes = [
 
  
   { path: 'login', component: LoginComponent },
+  { path: '#empresario', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   
 ];
 
