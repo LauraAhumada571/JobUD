2a89d26d5c4251e1cda2825b5582d740794feb16
Laura Ahumada <lauraahumada571@gmail.com>
2019-05-05 21:09:53 -0500

    funcion a bootstrap login

--- JobUD/src/app/app-routing.module.ts
+++ JobUD/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from '../app/Component/login/login.component';
+import { AppComponent } from './app.component';
+ 
+const routes: Routes = [];
+
+export const rootRouterConfig: Routes = [
+
+ 
+  { path: 'login', component: LoginComponent },
+  
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(rootRouterConfig)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
